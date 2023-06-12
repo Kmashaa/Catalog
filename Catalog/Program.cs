6643ed988b29d70cf8c5b30877c4ca6110eb80f0
kmashaa <m.kravcenko@yandex.by>
2023-06-12 15:18:13 +0300

    Add roles

--- Catalog/Program.cs
+++ Catalog/Program.cs
@@ -3,16 +3,38 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+//builder.Services.AddAuthorization(options =>
+//{
+//    options.AddPolicy("AdminOnly", policy => policy.RequireRole("admin"));
+//});
 
+//builder.Services.ConfigureApplicationCookie(options =>
+//{
+//    options.LoginPath = $"/Identity/Account/Login";
+//    options.LogoutPath = $"/Identity/Account/Logout";
+//});
+//builder.Services.AddIdentity<IdentityUser, IdentityRole>(opt =>
+//{
+//    opt.User.RequireUniqueEmail = true;
+//    opt.Password.RequireLowercase = false;
+//    opt.Password.RequireNonAlphanumeric = false;
+//    opt.Password.RequireUppercase = false;
+//    opt.Password.RequireDigit = false;
+//})
+//    .AddEntityFrameworkStores<ApplicationDbContext>()
+//    .AddDefaultTokenProviders();
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
+
 builder.Services.AddControllersWithViews();
+
 
 var app = builder.Build();
 
